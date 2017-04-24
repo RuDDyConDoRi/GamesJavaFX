de3236bdfa317a26fc76cef36bad53095e47656e
RuDDyConDoRi <rucocool@hotmail.com>
2017-04-24 22:18:27 +0200

    modificando elementos del Stage

--- src/gamesjavafx/controllers/ControllerSplash.java
+++ src/gamesjavafx/controllers/ControllerSplash.java
@@ -22,7 +22,7 @@ public class ControllerSplash extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
+        /*Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
@@ -30,21 +30,19 @@ public class ControllerSplash extends Application {
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
             }
-        });
+        });*/
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        //root.getChildren().add(btn);
         
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Enviando!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         launch(args);
     }
